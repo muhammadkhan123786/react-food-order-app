221d530fba86fd5f1ba507bfb64a6153d0f093fa
muhammadkhan123786 <muhammadkhan123829@gmail.com>
2024-12-26 16:18:09 +0500

    Some little issues in the code (Immutability issues resolved ) in reducer function..

--- src/store/cartItems/cartItemsReducers.js
+++ src/store/cartItems/cartItemsReducers.js
@@ -4,21 +4,20 @@ const initialState = { items: [] };
 
 const cartItemReducerFn = (state = initialState, action) => {
   if (action.type === addItem) {
-    let preItems = [...state.items];
+    let updatedItems = [...state.items];
     const isItemIndex = state.items.findIndex(
       (item) => item.id === action.item.id
     );
     if (isItemIndex > -1) {
       //item already exists.
-      preItems = preItems.map((item, index) => {
-        index === isItemIndex ? { ...item, quantity: item.quantity + 1 } : item;
-      });
-      return preItems;
+      updatedItems = updatedItems.map((item, index) =>
+        index === isItemIndex ? { ...item, quantity: item.quantity + 1 } : item
+      );
     } else {
       //new item comes.
-      preItems = [...state, { ...action.item, quantity: 1 }];
-      return preItems;
+      updatedItems = [...state.items, { ...action.item, quantity: 1 }];
     }
+    return { ...state, items: updatedItems };
   }
   return state;
 };
